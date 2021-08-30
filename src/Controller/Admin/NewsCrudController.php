e95f53d0688c44097eab76a023df9f58c4e91f20
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-08-30 13:26:21 +0200

    AdminController -> user crud controller

--- src/Controller/Admin/NewsCrudController.php
+++ src/Controller/Admin/NewsCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\FRONT\News;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class NewsCrudController extends AbstractCrudController
@@ -12,14 +14,13 @@ class NewsCrudController extends AbstractCrudController
         return News::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('title', 'Titre')->setHelp("Titre de la nouvelle."),
+            TextEditorField::new('content', 'Contenus')->setHelp("Contenus de la nouvelle"),
         ];
     }
-    */
+    
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\BACK\User;
+use Symfony\Component\Validator\Constraints\Regex;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -12,14 +20,46 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            EmailField::new('email', 'Adresse email')
+                ->setFormTypeOptions([
+                    'constraints' => [
+                        new NotBlank()
+                    ],
+                    'required' => true,
+                ]),
+            TextField::new('password', 'Mot de passe')
+                ->hideWhenUpdating()
+                ->setFormType(RepeatedType::class)
+                ->setFormTypeOptions([
+                    'type' => PasswordType::class,
+                    'invalid_message'   => 'Les mots de passe ne correspondent pas',
+                    'mapped'            => false,
+                    
+                    'required' => true,
+                    'first_options'    => [
+                        'label' => 'Votre Mot de passe',
+                        'constraints' => [
+                        new Regex('/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&-\/])[A-Za-z\d@$!%*#?&-\/]{10,}$/'),
+                        new NotCompromisedPassword()
+                    ],
+                    ],
+                    'second_options'    => [
+                        'label' => 'Confirmez votre Mot de passe',
+                    ]
+                ])
+                ->setLabel("Votre Mot de passe")
+                ->setHelp("Un mot de passe sécurisé contient : 10 caratères, des majuscules, minucules, chiffres et des caratères spéciaux."),
+                
+            ChoiceField::new('roles', 'Roles')
+                ->allowMultipleChoices()
+                ->setChoices([
+                "Utilisateur" => "ROLE_USER", 
+                "Administrateur" => "ROLE_ADMIN"]),
         ];
     }
-    */
+    
 }
