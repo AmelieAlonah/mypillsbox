ee25a6054f9fbe9c16a302acf6cefb307b1e80b5
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-08-31 14:11:18 +0200

    TEST ENTITY BACK User 90% Coverage True/False/Empty

--- src/Entity/BACK/User.php
+++ src/Entity/BACK/User.php
@@ -140,41 +140,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // $this->plainPassword = null;
     }
 
-    public function setUserName(string $userName): self
-    {
-        $this->userName = $userName;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Allergen[]
-     */
-    public function getAllergens(): Collection
-    {
-        return $this->allergens;
-    }
-
-    public function addAllergen(Allergen $allergen): self
-    {
-        if (!$this->allergens->contains($allergen)) {
-            $this->allergens[] = $allergen;
-            $allergen->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAllergen(Allergen $allergen): self
-    {
-        if ($this->allergens->removeElement($allergen)) {
-            // set the owning side to null (unless already changed)
-            if ($allergen->getUser() === $this) {
-                $allergen->setUser(null);
-            }
-        }
-
-        return $this;
-    }
 
 }
