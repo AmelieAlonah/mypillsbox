42ff5147e472980694539a5449210bbb8998acef
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-08-31 17:37:23 +0200

    TEST Controller ADMIM NewsCrudController

---
+++ tests/Controller/Admin/NewsCrudControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Controller\Admin;
+
+use App\Repository\BACK\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class NewsCrudControllerTest extends WebTestCase
+{
+    public function testAdminNews(): void
+    {
+        $client = static::createClient();
+
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('test@admin.fr');
+
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/admin');
+
+        $linkCrawler = $crawler->selectLink('News');
+
+        $this->assertResponseStatusCodeSame(302);
+    }
+}
