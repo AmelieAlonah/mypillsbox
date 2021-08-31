4bad62f091c2fc8c4b6c961ae6a10e596ecd8b00
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-08-31 18:58:01 +0200

    TEST [UPDATE] Controller FRONT RegisterController-> FormRegister, 50% coverage

--- tests/Controller/FRONT/RegisterControllerTest.php
+++ tests/Controller/FRONT/RegisterControllerTest.php
@@ -14,4 +14,30 @@ class RegisterControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Page d\'inscription');
     }
+
+    public function testRegisterAddGPOST(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/mon-compte/creation');
+
+        // $buttonCrawler = $crawler->selectButton('Ajouter');
+        // $form = $buttonCrawler->form();
+
+        // $form['register[email]'] = "email@email.fr";
+        // $form['register[password][first]'] = "Password";
+        // $form['register[password][second]'] = "Password";
+
+        // $client->submit($form);
+
+        // $crawler = $client->request('POST', '/mon-compte/creation');
+        // $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Ajouter', [
+            'register[email]' => "email@email.fr",
+            'register[password][first]' => "Password",
+            'register[password][second]' => "Password",
+        ]);
+
+        $this->assertResponseIsSuccessful();
+    }
 }
