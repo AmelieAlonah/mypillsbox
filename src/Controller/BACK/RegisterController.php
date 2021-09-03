9da3d38f9665aa219cad66bb42d29d9703985124
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-09-03 22:39:06 +0200

    Instal class mailer + extend + first param

--- src/Controller/BACK/RegisterController.php
+++ src/Controller/BACK/RegisterController.php
@@ -4,10 +4,12 @@ namespace App\Controller\BACK;
 
 use App\Entity\BACK\User;
 use App\Form\BACK\RegisterType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
@@ -17,7 +19,7 @@ class RegisterController extends AbstractController
     /**
      * @Route("/back-office/utilisateurs/ajout", name="back_office_user_add", methods={"GET", "POST"})
      */
-    public function registerAdd(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    public function registerAdd(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, MailerInterface $mailer): Response
     {
         $user = new User();
         
@@ -43,6 +45,17 @@ class RegisterController extends AbstractController
             $em->flush();
 
             $this->addFlash('success', 'L\'utilisateur a bien été ajouté.');
+
+            $mail = (new TemplatedEmail())
+                ->from('mypillsbox@gmail.com')
+                ->to($user->getEmail())
+                ->subject('Bienvenue sur MyPillsBox')
+                ->htmlTemplate('mailer/welcome.html.twig')
+                ->context([
+                    'user' => $user,
+                ]);
+
+            $mailer->send($mail);
 
             return $this->redirectToRoute('back_office_home');
         }
