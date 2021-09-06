fbf2b481d2fe30fd5e2fe994a5f12e6c2ad053a0
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-09-06 13:01:25 +0200

    Fix mailer

--- src/Controller/FRONT/RegisterController.php
+++ src/Controller/FRONT/RegisterController.php
@@ -4,7 +4,9 @@ namespace App\Controller\FRONT;
 
 use App\Entity\BACK\User;
 use App\Form\FRONT\RegisterType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,7 +31,7 @@ class RegisterController extends AbstractController
     /**
      * @Route("/mon-compte/creation", name="account_add", methods={"GET", "POST"})
      */
-    public function registerAdd(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    public function registerAdd(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, MailerInterface $mailer): Response
     {
         $user = new User();
         
@@ -56,6 +58,17 @@ class RegisterController extends AbstractController
             $em->flush();
 
             $this->addFlash('success', 'Votre compte a bien été enregistré.');
+
+            $mail = (new TemplatedEmail())
+                ->from('mypillsbox@gmail.com')
+                ->to($user->getEmail())
+                ->subject('Bienvenue sur MyPillsBox')
+                ->htmlTemplate('mailer/welcome.html.twig')
+                ->context([
+                    'user' => $user,
+                ]);
+
+            $mailer->send($mail);
 
             return $this->redirectToRoute('home');
         }
