4b5cd520e91691cda9c6b15bb0eca9a3b03431f3
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-09-07 10:43:28 +0200

    cache clear prod

--- src/Controller/Admin/MedicineCrudController.php
+++ src/Controller/Admin/MedicineCrudController.php
@@ -5,11 +5,9 @@ namespace App\Controller\Admin;
 use App\Entity\BACK\Medicine;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Type\TextEditorType;
 
 class MedicineCrudController extends AbstractCrudController
 {
